80c852dcf4e0db8dd6c08c344fc7cc99495ec8f7
unknown <shaikhaala@Live.com>
2020-06-15 08:03:40 -0700

    did the task

--- story.js
+++ story.js
@@ -1,7 +1,11 @@
-// Define the name here on line 2
-
+let name = "shaikha alammari";
 // Define the age here on line 4
-
+let age = 25;
 // Define the hobby here on line 6
-
+let hobby = "exploring the world";
 // Print the story here on line 8
+console.log(
+  `ms. ${name} in 33 years you will be ${
+    age + 33
+  }. by then you will have decades  of ${hobby} practice`
+);
